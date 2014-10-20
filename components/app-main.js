471e91892ad8858b10ccc75522dc644875e2ae0f
Bryce Thorup <brycethorup@gmail.com>
2014-10-20 12:54:15 -0600

    Added overlays and ability to change them.

--- components/app-main.js
+++ components/app-main.js
@@ -1,4 +1,55 @@
 Polymer({
 
+  overlayIndex: 1,
+
+  ready: function(){
+
+    this.overlays = [
+
+      "beard_guy",
+
+      "big_hair_lady",
+
+      "egypt_lady"
+
+    ]
+
+    document.addEventListener("keyup", function(e){
+
+      if(e.keyCode == 39){
+
+        if(this.overlayIndex + 1 == this.overlays.length){
+
+          this.overlayIndex = 0;
+
+        }
+
+        else{
+
+          this.overlayIndex++;
+
+        }
+
+      }
+
+      else if(e.keyCode == 37){
+
+        if(this.overlayIndex - 1 == -1){
+
+          this.overlayIndex = this.overlays.length - 1;
+
+        }
+
+        else{
+
+          this.overlayIndex--;
+
+        }
+
+      }
+
+    }.bind(this))
+
+  }
 
 });
